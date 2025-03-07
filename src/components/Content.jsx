d0405471266924aa0e906e3ea8560253690d9904
Manoj-M19 <manojpavithra2004@gmail.com>
2025-03-07 16:06:10 +0000

    added signup page

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,50 +1,14 @@
 import React from 'react'
+import Signup from './Signup'
 
 export default function Content() {
     return (
         <>
-            <main className="flex-grow p-4"> {/* Main content expands */}
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae atque nam officia, natus inventore praesentium delectus rem vitae similique quibusdam architecto excepturi eligendi aspernatur magni nihil dolorem ad, optio laudantium.
-                Ex architecto unde dolore asperiores. Officia quae obcaecati eaque doloribus ducimus placeat aut laborum dolorem quas nihil libero ea, iusto ratione eligendi eius, perspiciatis quos molestiae aliquid distinctio? Alias, explicabo?
-                Voluptatum exercitationem minus dolorem corrupti rem omnis molestias facere similique. Iste est, illo porro incidunt odio, ipsa blanditiis quod culpa impedit inventore molestiae minima rerum quibusdam repellendus fugit ipsam voluptatem?
-                Molestias, sint iure. Nostrum eveniet reiciendis repellat magni molestias possimus ipsum animi vitae ipsa totam, rem quia veritatis architecto id voluptatibus fugit! Vero minima repudiandae dolores tenetur cupiditate veritatis sunt?
-                Illum asperiores, maxime libero error et ipsam molestias hic perspiciatis deleniti adipisci consequatur quaerat assumenda. Soluta nobis exercitationem similique, accusantium sunt dolorum incidunt, esse possimus id quas eius sit at!
-                Possimus deleniti officiis quo molestias minima quos non. Iure nihil odit eaque molestias delectus quibusdam dolorum incidunt! Quod iste nihil, accusamus, officia explicabo, veritatis earum expedita quasi totam minus necessitatibus?
-                Aperiam eligendi ipsum vel modi illo velit impedit eaque. Laboriosam a porro facilis inventore ducimus, expedita accusantium culpa modi voluptatem fugiat maiores est aut odio eos ipsa excepturi corrupti vitae.
-                Quo earum libero, voluptas dignissimos rem quisquam neque quidem. Voluptate fuga facere officiis voluptatum quam. Deleniti, eius? Enim, alias. Veritatis odio eaque, ut qui id dolorem quis accusamus recusandae aliquam!
-                Praesentium in nihil accusamus culpa. Quisquam similique soluta, accusantium earum reprehenderit blanditiis quod beatae quaerat suscipit officia nobis, voluptatibus inventore, aliquam quas asperiores? Eius molestiae ratione saepe perferendis iusto nam?
-                Ad iure consectetur ab placeat, doloremque labore! Obcaecati sit molestias, excepturi veniam mollitia et quidem cum nisi error, perspiciatis reiciendis eum repellendus fugiat, corrupti velit commodi accusantium? Similique, odio quia.
-                Voluptatem ducimus et doloribus saepe optio quos, culpa quibusdam illum quo accusantium eos recusandae ad repudiandae autem nesciunt omnis officiis dignissimos quod officia fugiat ratione aliquam itaque labore facere. Iste.
-                Molestias atque veritatis sit laborum, ipsa pariatur ducimus dolorem commodi laudantium eligendi itaque enim libero quo aliquid sapiente harum qui delectus animi facilis fugiat, quibusdam repellendus debitis. Rem, a optio?
-                Expedita repellat odio fugiat beatae, dignissimos, optio suscipit eveniet nihil qui cumque nesciunt, ducimus sed sit quidem debitis veniam. Corporis saepe amet minima obcaecati ipsam consequatur, commodi nisi magni inventore!
-                Neque quia veritatis, repellendus, accusantium, tenetur nobis quod fugiat perferendis exercitationem placeat cum unde. Ipsa, laudantium! Officia quo ipsum minus tempora aliquam, amet animi, vitae, accusantium voluptates unde nulla iusto.
-                Dolorum suscipit expedita minima mollitia molestiae necessitatibus iste, labore dolore, quis, eos inventore modi ratione sit error commodi in repudiandae eum. Eius dolor ullam vel tenetur aperiam illum laboriosam debitis.
-                Quos amet perferendis, a vitae id in nemo iure. Tempora fugit sed at libero, quasi iste repellat magnam possimus reiciendis magni aliquid, in unde inventore qui eum ullam ex suscipit.
-                Mollitia iusto laborum impedit nemo fuga, eius libero tenetur perferendis. Illo, saepe? Sint ratione aperiam quod soluta in, eligendi impedit est dignissimos et quas itaque. Obcaecati accusamus hic quis ipsa.
-                Harum accusamus, reprehenderit, molestias obcaecati neque porro cupiditate voluptates minus dolorem sapiente totam, sed consectetur explicabo! Modi, itaque iure consequuntur odit laborum quas sed recusandae vero earum magni. Vel, deserunt!
-                Repellendus, laboriosam? Sunt provident iusto amet natus possimus ex, voluptatem nesciunt ratione non incidunt dicta maiores voluptatibus sed, animi quos, magni minus delectus commodi blanditiis. Voluptatem ex ut fugiat. Quidem!
-                Dolorum perferendis aspernatur excepturi sed nam soluta neque inventore itaque voluptates dicta quibusdam, labore consectetur, modi fugit! Rerum laboriosam harum magnam totam iste odit qui delectus voluptates a, amet nesciunt!
-                Maxime dolore recusandae totam magni sint dolor ducimus voluptas nostrum? Iste commodi enim laboriosam voluptates quas perferendis! Nemo, voluptates? Pariatur vero repellat saepe, molestiae laboriosam adipisci vitae dolore voluptas assumenda?
-                Debitis omnis id minus amet itaque, quasi quod suscipit quisquam nisi, ut labore commodi consequuntur dolorum adipisci aut officiis cum similique quae temporibus repudiandae neque saepe. Quibusdam, illum atque! Iusto.
-                Impedit consectetur sunt odio, neque ex enim non consequatur totam veniam harum provident, obcaecati quo. Dolorem praesentium deleniti non recusandae hic unde aliquam maiores tenetur harum ex. Quos, enim necessitatibus.
-                Fugit molestias id, provident deleniti delectus nostrum eaque. Earum assumenda deserunt id sequi obcaecati impedit accusamus, tenetur similique. Reiciendis optio iusto hic deserunt in quod cum accusamus mollitia assumenda earum?
-                Soluta officia sit praesentium quod at perspiciatis explicabo aspernatur quia dicta dolorem alias deleniti, repudiandae nisi harum. Obcaecati similique voluptatibus, repudiandae enim adipisci aut autem asperiores distinctio sunt, recusandae odio.
-                Optio dolor nam doloribus. Quia sint id quas tenetur sit eligendi accusantium iusto quis minima aliquam quam, illum commodi totam nihil magni fugit fuga. Tenetur maiores sequi ab necessitatibus laboriosam?
-                Quia corrupti saepe accusantium non, in laborum voluptatum, quasi accusamus adipisci consequatur, doloremque error asperiores at natus nisi voluptas eveniet nostrum veritatis itaque. Accusamus delectus iste, voluptas neque eaque doloribus.
-                Laborum, repellendus temporibus. Exercitationem, harum. Voluptatum, aspernatur consectetur eveniet tempore itaque magni exercitationem, quo nam qui excepturi eaque rerum minus assumenda consequuntur velit quam voluptate odit ratione neque atque. Consequuntur!
-                Sit, magni. Dolor quasi reiciendis nulla, perferendis molestiae eius dicta sed hic commodi harum numquam asperiores minima ab eligendi ullam, tenetur beatae maiores consectetur sunt molestias? Eaque in explicabo laboriosam!
-                Asperiores ut, esse error similique soluta dolore saepe natus non ipsa alias veniam quia, corrupti id officia odio voluptas molestiae? Aliquam error assumenda quae voluptatibus fugiat iusto ullam alias exercitationem?
-                Ullam, sed. Perferendis consectetur modi et illo quos non ipsum quae consequatur nesciunt eveniet facere reprehenderit, doloremque cum obcaecati ullam fuga iusto voluptas facilis? Aut repellat temporibus voluptate est mollitia.
-                Commodi accusantium voluptatem reprehenderit, harum deserunt suscipit beatae quidem corrupti, veniam expedita ex officiis vero? Inventore repellat ipsum a sint aspernatur debitis eum sequi. Accusantium, animi praesentium? Rem, a reprehenderit.
-                Aut eveniet quidem, unde explicabo quo, quibusdam dolorem cum modi minus consectetur exercitationem enim nihil cupiditate est. Fuga soluta fugit molestiae consequatur ipsa numquam suscipit veritatis, ullam tempora id cumque!
-                Quod nobis nostrum sunt laboriosam distinctio earum consequatur ipsam quas dolore vitae! Animi, suscipit ea? Excepturi eius amet quo ipsam adipisci cumque atque. Eum accusamus dolorum ex laborum, animi sint.
-                Ipsa a saepe tempore vero nisi. Facilis at rem sapiente blanditiis velit qui dolor, minus odit quos esse laborum minima repellat consequatur ex eius culpa necessitatibus? Nihil id similique rem.
-                Libero reprehenderit dolores ea obcaecati molestiae similique eius commodi. Incidunt obcaecati ratione voluptatem repudiandae, numquam praesentium a nisi molestias. Aliquam in ducimus, nostrum vero sequi ullam architecto. Nam, quidem vitae!
-                Voluptatum, nam impedit veritatis sit facilis ex ea itaque, omnis non nesciunt maiores! Expedita ullam, quam totam, officia est sint veritatis deserunt natus mollitia pariatur ab vero rerum, aut repellat!
-                Doloremque accusantium dolore maiores quam est veritatis ea? Perferendis, atque, provident debitis architecto accusantium, magni iusto odit molestiae sapiente voluptas vel beatae officiis omnis quidem numquam labore. Perferendis, repellat doloribus!
-                Voluptas sapiente at, eaque ipsa corporis sit doloribus deleniti itaque temporibus optio? Officia, eius minus praesentium quam totam quibusdam! Odit voluptas earum distinctio, unde minus necessitatibus vitae hic architecto eius.
-                Itaque molestias temporibus magni repellendus alias ipsa soluta dicta officia, earum velit, impedit nam architecto sed vero quo, repudiandae eius laudantium delectus pariatur numquam placeat laborum eos vel praesentium? Quibusdam.
-            </main>
+            <main className="flex-grow "> {/* Main content expands */}
+               <Signup>
+
+               </Signup>
+             </main>
         </>
     )
 }
